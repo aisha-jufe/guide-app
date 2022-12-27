9943b98dc16cc5578a9cf4cf27c88dd1438aefc7
aisha-jufe <aishajucfe287>
2022-12-27 22:17:51 +0300

    adds event listeners to button for action on click

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", getParks())
 
-// Query and assign to variables found nodes
-const parkList = document.querySelector(".list-container");
+// Query and assign to variables usefull nodes
+const parkList = document.querySelector("#parks");
 const parkImage = document.querySelector("#image");
 const parkImage2 = document.querySelector("#image2");
 const parkName = document.querySelector("#name");
@@ -10,17 +10,14 @@ const parkdescription = document.querySelector("#park-info");
 const parkFee = document.querySelector("#entry-fee");
 const parkFeatures = document.querySelector("#features");
 const parkAmenities= document.querySelector("#amenities");
-const bookPark= document.querySelector("buy-ticket");
+const bookPark = document.getElementById('buy-ticket')
 const visitedPark = document.querySelector("#visited");
 
-//global variable for currently selected park  
+//variable for currently selected park, that will be available in the global scope
 let currentPark;
 
-//clear park 
-parkList.innerText = '';
 
-
-// fetch parks and displays the first park on DOMContentLoaded
+// fetch parks and displays the first park on site visit
 function getParks () {
   fetch('https://test-backend-production-30ff.up.railway.app/parks')
     .then(r => r.json())
@@ -28,23 +25,22 @@ function getParks () {
   starter();
 }
   
-// Fetch the first park to display when page loads
-function starter () {
-  
+// Function to fetch the first park
+function starter () {  
   fetch('https://test-backend-production-30ff.up.railway.app/parks/1')
     .then(r => r.json())
     .then(showPark);
 }
   
-  // Iterate over the array of parks to access individual parks
-  function renderParks (parks) {
-    parks.forEach(renderPark);
-  }
+// Iterate over the array of parks(payload) to access individual parks
+function renderParks (parks) {
+  parks.forEach(renderPark);
+}
   
-// takes in a park object and accesses individual values
+// Takes in a park object and accesses individual values
 function renderPark (park) {
-  const parkN = document.createElement('div');
-  parkN.setAttribute('id', 'park-names');
+  const parkN = document.createElement('li');
+  parkN.setAttribute('class', 'park item');
   parkN.style.cursor = 'pointer';
   parkN.innerHTML = park.name;
   parkList.appendChild(parkN);
@@ -54,18 +50,45 @@ function renderPark (park) {
 }
   
 // responsible for displaying details about a given park to the user
-function showPark (park) {
-  
+function showPark (park) {  
+  currentPark = park;
   parkName.innerText = park.name;
   parkLocation.innerText = park.location;
   parkdescription.innerText = park.description;
   parkFee.innerHTML = `Entry fee: ${park.entryFee}`;
-  parkFeatures.innerHTML = `<e>Main Features</e> <br> ${park.features}`;
-  parkAmenities.innerHTML = `Nearby Amenities: <br> ${park.amenities}`
+  parkFeatures.innerHTML = `<span style="color: black; font-size:medium; font-weight: 700;">Main Features</span> <br> ${park.features}`;
+  parkAmenities.innerHTML = `<span style="color: black; font-size:medium; font-weight: 700;">Nearby Amenities</span><br> ${park.amenities}`
   image.src = park.image;
   parkImage2.src = park.image2
-
-  
-  // Event listener if one has visited park, to delete it from their view
 }
-  
+
+// Event listener if one has visited park, to delete it from their view.
+// Note that this is not persisted on the backend as it would alter stored destionations.
+// For contuinity, this will hide the park and display the next park on the list
+visitedPark.addEventListener("click", (e)=>{
+  e.preventDefault();
+  parkReplace();
+})
+
+function parkReplace(){
+  const nextParkId = currentPark.id + 1;
+  if (nextParkId > 13){
+    fetch(`https://test-backend-production-30ff.up.railway.app/parks/${nextParkId}`)
+    .then(r => r.json())
+    .then(showPark)
+  } else{
+    alert('You have visited All the top destinations!')
+    starter();
+  }
+}
+
+// Book visit will generate a pop-up alert and alter the status to booked.
+bookPark.addEventListener('click', (e)=>{
+  e.preventDefault();
+  booker();
+})
+
+function booker() {
+  bookPark.innerHTML = "Booked!"
+  alert(`You have booked a visit to ${currentPark.name}`)
+}
